2528f9f2a19d31cda41ec0e368669d87af494260
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-03 23:55:35 +0700

    feat: dashboard (not fix)

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,19 +1,77 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/inertia-react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Inertia } from "@inertiajs/inertia";
+import { Head } from "@inertiajs/inertia-react";
+import { useState } from "react";
 
 export default function Dashboard(props) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = () => {
+        const data = {
+            title,
+            description,
+            category,
+        };
+        Inertia.post("/news", data);
+        setTitle("");
+        setDescription("");
+        setCategory("");
+    };
+
+    console.log(props);
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="flex flex-col gap-4 m-10">
+                            <h1>Masukan Berita</h1>
+                            <input
+                                type="text"
+                                placeholder="Title"
+                                className="input input-bordered w-full"
+                                onChange={(title) =>
+                                    setTitle(title.target.value)
+                                }
+                                value={title}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Description"
+                                className="input input-bordered w-full"
+                                onChange={(description) =>
+                                    setDescription(description.target.value)
+                                }
+                                value={description}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Category"
+                                className="input input-bordered w-full"
+                                onChange={(category) =>
+                                    setCategory(category.target.value)
+                                }
+                                value={category}
+                            />
+                            <button
+                                className="btn"
+                                onClick={() => handleSubmit()}
+                            >
+                                Submit
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
