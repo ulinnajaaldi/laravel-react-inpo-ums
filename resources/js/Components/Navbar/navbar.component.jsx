6fbbc770e26ae41189648fb8fad3ef0f02d16b4b
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-04 20:53:40 +0700

    style: change title name

--- resources/js/Components/Navbar/navbar.component.jsx
+++ resources/js/Components/Navbar/navbar.component.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ user }) {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Inpo UMS</a>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
