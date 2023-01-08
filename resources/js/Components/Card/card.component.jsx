f5f5fd3c17b53b5ee755aa5ebe38d69dfd4ca2fd
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-08 23:08:53 +0700

    feat: change layout card component

--- resources/js/Components/Card/card.component.jsx
+++ resources/js/Components/Card/card.component.jsx
@@ -4,26 +4,51 @@ export default function Card({ news }) {
     const isNews = (news) => {
         return news.map((data, index) => {
             return (
-                <div
-                    className="card w-full md:w-96 bg-base-100 shadow-xl"
-                    key={index}
-                >
-                    <figure>
-                        <img
-                            src="https://placeimg.com/400/225/arch"
-                            alt="Shoes"
-                        />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{data.title}</h2>
-                        <p>{data.description}</p>
-                        <p>{data.author}</p>
-                        <div className="card-actions justify-end">
-                            <div className="badge badge-outline">
-                                {data.category}
+                <div className="" key={index}>
+                    <div className="flex gap-4 border shadow-md p-4 rounded-xl">
+                        <div className="btn-disabled w-10 h-10 rounded-full flex items-center justify-center bg-accent-focus text-white mt-1">
+                            {(
+                                data.author.split("@")[0].charAt(0) +
+                                data.author
+                                    .split("@")[0]
+                                    .charAt(
+                                        data.author.split("@")[0].length - 1
+                                    )
+                            ).toUpperCase()}
+                        </div>
+                        <div className="flex flex-col w-[800px] ">
+                            <div className="flex gap-2 items-center">
+                                <h2 className="font-bold text-lg">
+                                    {data.title}
+                                </h2>
+                                <p className="font-extralight text-sm">
+                                    {data.author}
+                                </p>
+                            </div>
+                            <div className="flex flex-col gap-5">
+                                <div>{data.description}</div>
+                                <div className="card-actions justify-start">
+                                    <div className="badge badge-outline text-xs">
+                                        {data.category}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    {/* <div className="flex items-start justify-center gap-2">
+                        <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card-body">
+                                <h2 className="card-title">{data.title}</h2>
+                                <p>{data.description}</p>
+                                <p>{data.author}</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">
+                                        {data.category}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
             );
         });
