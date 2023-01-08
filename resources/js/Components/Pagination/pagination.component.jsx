6cc41a9493aebe3bd15000fb2ff3d824bfbe415f
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-08 23:08:34 +0700

    feat: fix pagination

--- resources/js/Components/Pagination/pagination.component.jsx
+++ resources/js/Components/Pagination/pagination.component.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Link } from "@inertiajs/inertia-react";
 
 export default function Pagination({ meta }) {
-    const prev = meta.links[0].url;
-    const next = meta.links[meta.links.length - 1].url;
+    const prev = meta.links[0] ? meta.links[0].url : null;
+    const next = meta.links[meta.links.length - 1]
+        ? meta.links[meta.links.length - 1].url
+        : null;
     const current = meta.current_page;
+    const lastPage = meta.last_page;
 
     return (
-        <div className="btn-group">
-            <Link href={prev} className="btn ">
+        <div className="btn-group m-5">
+            <Link href={prev} className="btn " disabled={current === 1}>
                 «
             </Link>
-            <button className="btn ">{current}</button>
-            <Link href={next} className="btn ">
+            <button className="btn ">Page {current}</button>
+            <Link href={next} className="btn " disabled={current === lastPage}>
                 »
             </Link>
         </div>
