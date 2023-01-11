0680c7d2b66b78f426c7905c596763da580c9fc6
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-12 03:14:09 +0700

    feat: fix navbar component

--- resources/js/Components/Navbar/navbar.component.jsx
+++ resources/js/Components/Navbar/navbar.component.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "@inertiajs/inertia-react";
 import { BiSun, BiMoon } from "react-icons/bi";
 import { IoEllipsisHorizontalSharp } from "react-icons/io5";
+import { BsPeople } from "react-icons/bs";
 import "./navbar.css";
 
 export default function Navbar({ user }) {
@@ -19,59 +20,70 @@ export default function Navbar({ user }) {
     }, [theme]);
 
     return (
-        <div className="h-screen bg-base-100 shadow-md px-5 py-3 sticky top-0 z-40 flex flex-col">
-            <div className="flex-1 my-4">
-                <a className="px-5 py-3 text-xl font-extrabold hover:bg-base-200 hover:rounded-full cursor-default">
+        <div className="h-screen bg-base-100 shadow-md px-5  sticky top-0 z-40 flex flex-col justify-between pt-3 pb-20">
+            <div className="flex items-center justify-between">
+                <Link
+                    href="/"
+                    className="px-5 py-3 text-xl font-extrabold hover:bg-base-200 hover:rounded-full cursor-default"
+                >
                     Inpo UMS
-                </a>
+                </Link>
+                <button onClick={toggleTheme} className="pr-5">
+                    {theme === "dark" ? (
+                        <div className="p-2 hover:bg-base-200 rounded-full">
+                            <BiMoon size={25} />
+                        </div>
+                    ) : (
+                        <div className="p-2 hover:bg-base-200 rounded-full">
+                            <BiSun size={25} />
+                        </div>
+                    )}
+                </button>
             </div>
-            <div className="flex-none gap-4">
-                <div className="flex items-center justify-start ">
-                    <button onClick={toggleTheme}>
-                        {theme === "dark" ? (
-                            <div className="flex gap-2 px-5 py-3 hover:bg-base-200 hover:rounded-full">
-                                <BiMoon size={25} />
-                                <p>Change to Light</p>
-                            </div>
-                        ) : (
-                            <div className="flex gap-2 px-5 py-3 hover:bg-base-200 hover:rounded-full">
-                                <BiSun size={25} />
-                                <p>Change to Dark</p>
-                            </div>
-                        )}
-                    </button>
-                </div>
-
+            <div className="flex flex-col gap-2">
                 {!user ? (
-                    <div className="dropdown dropdown-right dropdown-end">
-                        <label
-                            tabIndex={0}
-                            className="flex gap-4 items-center px-5 py-3 hover:bg-base-200 hover:rounded-full"
+                    <div>
+                        <Link
+                            href="about"
+                            className="flex gap-4 px-5 py-3 hover:bg-base-200 hover:rounded-full"
                         >
-                            <div className="w-10 h-10 rounded-full flex items-center justify-center">
-                                <img src="https://i.ibb.co/w6nbqQr/image.png" />
-                            </div>
-                            <div>
-                                <p className="text-sm">Any information?</p>
-                            </div>
-                        </label>
-                        <ul
-                            tabIndex={0}
-                            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52"
-                        >
-                            <div>
-                                <li>
-                                    <Link href={route("login")} as="button">
-                                        Login
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href={route("register")} as="button">
-                                        Register
-                                    </Link>
-                                </li>
-                            </div>
-                        </ul>
+                            <BsPeople size={25} />
+                            <p>About Us</p>
+                        </Link>
+
+                        <div className="dropdown dropdown-right dropdown-end">
+                            <label
+                                tabIndex={0}
+                                className="flex gap-4 items-center px-5 py-3 hover:bg-base-200 hover:rounded-full"
+                            >
+                                <div className="w-10 h-10 rounded-full flex items-center justify-center">
+                                    <img src="https://i.ibb.co/w6nbqQr/image.png" />
+                                </div>
+                                <div>
+                                    <p className="text-sm">Any information?</p>
+                                </div>
+                            </label>
+                            <ul
+                                tabIndex={0}
+                                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52"
+                            >
+                                <div>
+                                    <li>
+                                        <Link href={route("login")} as="button">
+                                            Login
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            href={route("register")}
+                                            as="button"
+                                        >
+                                            Register
+                                        </Link>
+                                    </li>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                 ) : (
                     <div className="dropdown dropdown-right dropdown-end">
@@ -115,7 +127,10 @@ export default function Navbar({ user }) {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link href="" as="button">
+                                    <Link
+                                        href={route("profile.edit")}
+                                        as="button"
+                                    >
                                         Settings
                                     </Link>
                                 </li>
