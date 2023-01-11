575ccf753b55820e787ff21c93400ef7c850267a
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-11 23:35:36 +0700

    feat: card auth component(not fix)

--- resources/js/Components/Card/card.auth.component.jsx
+++ resources/js/Components/Card/card.auth.component.jsx
@@ -2,38 +2,48 @@ import { Link } from "@inertiajs/inertia-react";
 import React from "react";
 
 export default function CardAuth(props) {
-    console.log("card auth: ", props);
+    const category = props.category.split(" ");
+
     return (
-        <div className="card w-full md:w-96 bg-base-100 shadow-xl">
-            <figure>
-                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{props.title}</h2>
-                <p>{props.description}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">{props.category}</div>
+        <div className="flex items-center justify-between gap-4 border shadow-md p-4 rounded-xl">
+            <div className="flex flex-col w-[800px] ">
+                <div className="flex gap-2 items-center">
+                    <h2 className="font-bold text-lg">{props.title}</h2>
+                    <p className="font-extralight text-sm">{props.author}</p>
                 </div>
-                <div className="flex gap-3">
-                    <Link
-                        href={route("edit.news")}
-                        method="get"
-                        data={props.dataEdit}
-                        className="btn btn-success"
-                        as="button"
-                    >
-                        Edit
-                    </Link>
-                    <Link
-                        href={route("delete.news")}
-                        method="post"
-                        data={props.dataDelete}
-                        className="btn btn-success"
-                        as="button"
-                    >
-                        Delete
-                    </Link>
+                <div className="flex flex-col gap-5">
+                    <div>{props.description}</div>
+                    <div className="card-actions justify-start">
+                        {category.map((data, index) => (
+                            <div
+                                className="badge badge-outline text-xs"
+                                key={index}
+                            >
+                                {data}
+                            </div>
+                        ))}
+                    </div>
                 </div>
+            </div>
+            <div className="flex justify-end gap-3">
+                <Link
+                    href={route("edit.news")}
+                    method="get"
+                    data={props.dataEdit}
+                    className="btn btn-warning hover:scale-90"
+                    as="button"
+                >
+                    Edit
+                </Link>
+                <Link
+                    href={route("delete.news")}
+                    method="post"
+                    data={props.dataDelete}
+                    className="btn btn-error hover:scale-90"
+                    as="button"
+                >
+                    Delete
+                </Link>
             </div>
         </div>
     );
